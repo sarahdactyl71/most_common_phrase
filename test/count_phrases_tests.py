cdf809c2910462dac1c739825b450fc6cb99ce9d
Sarah Kirk <kirkse710@gmail.com>
2020-11-28 12:16:55 -0800

    adds more tests

--- test/count_phrases_tests.py
+++ test/count_phrases_tests.py
@@ -38,3 +38,21 @@ def test_cleaned_words_from_file_two():
   random_section = large_file[776:786]
   cleaned_list =  clean_word_list(random_section)
   assert_equal(cleaned_list, ['inorganic','world','being','the','result','of','law','and','not','of'])
+
+#testing for count_common_phrases
+def test_count_common_phrases_from_given_list():
+  phrases = ["i", "like", "dogs", "animals", "sushi", "is", "tasty", "finn", "i", "like", "dogs","i", "like", "dogs", "octopus", "i", "like", "dogs", "corgi", "sushi", "is", "tasty"]
+  common_phrases = count_common_phrases(phrases)
+  assert_equal(common_phrases[0], ('i like dogs', 4))
+  assert_equal(common_phrases[1], ('sushi is tasty', 2))
+
+def test_count_common_phrases_from_file():
+  file = ['origin_of_species.txt']
+  large_file = get_words_from_files(file)
+  cleaned_list =  clean_word_list(large_file)
+  phrase = count_common_phrases(cleaned_list)
+  assert_equal(phrase[0], ('of the same', 320))
+  assert_equal(phrase[1], ('the same species', 130))
+  assert_equal(phrase[2], ('conditions of life', 125))
+  assert_equal(phrase[3], ('in the same', 117))
+
