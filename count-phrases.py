de323274d9d21e2564f1391232bbf58b23c59d71
Sarah Kirk <kirkse710@gmail.com>
2020-11-27 17:01:58 -0800

    worked through stdin solution

--- count-phrases.py
+++ count-phrases.py
@@ -1,8 +1,12 @@
+import sys
 
 def get_words_from_files(text_file_path):
   words = []
-  for files in text_file_path:
-    words += open(files).read().lower().split()
+  if not sys.stdin.isatty():
+    words = sys.stdin.read().lower().split()
+  else:
+    for files in text_file_path:
+      words += open(files).read().lower().split()
   return words
 
 def count_common_phrases(text_file_path):
@@ -26,9 +30,15 @@ def formats_output(text_file_path):
   for pair in popular_phrase:
     print "{} - {}".format(pair[0], pair[1])
 
+if not sys.stdin.isatty():
+  stdin_file = sys.stdin
+  formats_output(stdin_file)
 
-formats_output(['../origin_of_species.txt', '../moby_dick.txt'])
+# formats_output(['../origin_of_species.txt', '../moby_dick.txt'])
+# formats_output(['../moby_dick.txt'])
 
 # species length = 209750
 # moby length = 215830
 # import pdb; pdb.set_trace()
+
+# cat ../moby_dick.txt | python ./count-phrases.py
